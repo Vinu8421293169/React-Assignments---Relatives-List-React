b891cdb47a1adb728c192e33f1d4daa3e7a8419a
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-05 10:10:31 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          <li key="relativeListItem1">Grandfather</li>
+          <li key="relativeListItem2">Uncle</li>
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
